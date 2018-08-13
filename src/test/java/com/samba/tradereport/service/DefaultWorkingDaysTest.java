4bc4bcde4d5b48262401253a148815b45ed3ad97
Samba Mitra <samba.mitra@nhs.net>
2018-08-13 09:39:20 +0100

    improve test assertions and rename test methods

--- src/test/java/com/samba/tradereport/service/DefaultWorkingDaysTest.java
+++ src/test/java/com/samba/tradereport/service/DefaultWorkingDaysTest.java
@@ -1,7 +1,11 @@
 package com.samba.tradereport.service;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 import org.junit.Before;
@@ -17,15 +21,30 @@ public class DefaultWorkingDaysTest {
 	}
 
 	@Test
-	public void testGetNextWorkingDay() {
-		final LocalDate nextWorkingDay = this.workingDays.getNextWorkingDay(LocalDate.now());
-		assertNotNull("Next working day not expected to be null", nextWorkingDay);
+	public void nextWorkingDayShouldReturnCurrentDayWhenCurrentDayIsWorkDay() {
+		// Given - current day is Monday
+		final LocalDate expected = LocalDate.of(2018, 8, 13);
+
+		// When
+		final LocalDate actual = this.workingDays.getNextWorkingDay(expected);
+
+		// Then
+		assertNotNull("Next working day not expected to be null", actual);
+		assertEquals("Next working day should be the same as the current day", expected, actual);
 	}
 
 	@Test
-	public void testGetNextWorkingDayForWeekend() {
-		final LocalDate nextWorkingDay = this.workingDays.getNextWorkingDay(LocalDate.of(2018, 8, 4));
-		assertNotNull("Next working day not expected to be null", nextWorkingDay);
+	public void nextWorkingDayShouldReturnNextDayWhenCurrentDayIsWeekend() {
+		// Given - current day is Sunday
+		final LocalDate expected = LocalDate.of(2018, 8, 12);
+
+		// When
+		final LocalDate actual = this.workingDays.getNextWorkingDay(expected);
+
+		// Then - next working day should be Monday
+		assertNotNull("Next working day not expected to be null", actual);
+		assertThat("Next working day should be the day after the current day", actual.getDayOfWeek(),
+				is(DayOfWeek.MONDAY));
 	}
 
 }
